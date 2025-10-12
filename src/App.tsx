6d258440e536dd9f1663035a53356e59f35627af
RiyanTriadi <riyantriadii@gmail.com>
2025-10-12 14:09:40 +0700

    Refactor

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ListGroup from "./components/ListGroup";
+
+function App () {
+    const items = ["Washington DC", "Jakarta", "Tokyo", "Seoul", "Kuala Lumpur"];
+    const handeSelectItem = (item: string) => {
+      console.log(item)
+    }
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handeSelectItem}/></div>
+}
+
+export default App;
