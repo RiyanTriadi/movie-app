1b9dc8001fa8c713bcef0ba9465792bb4a10fb8f
RiyanTriadi <riyantriadii@gmail.com>
2025-10-13 12:28:41 +0700

    Minor Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import ContactPage from './pages/ContactPage';
-import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import ContactPage from "./pages/ContactPage";
+import Navbar from "./components/Navbar";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false)
+
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/profile' element={<ProfilePage/>}/>
-        <Route path='/contact' element={<ContactPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
+      {alertVisible && <Alert onClose={()=>setAlertVisibility(false)}>This is an Alert</Alert>}
+      <Button onClick={()=> {setAlertVisibility(true)}}>Klik Disini</Button>
     </div>
   );
 }
