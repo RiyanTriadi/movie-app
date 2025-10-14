1681ad746809776f0f40b9b7cc12c838148d0a2e
RiyanTriadi <riyantriadii@gmail.com>
2025-10-14 17:58:01 +0700

    Add Features

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import ContactPage from "./pages/ContactPage";
 import Navbar from "./components/Navbar";
-import Button from "./components/Button";
-import Alert from "./components/Alert";
-import { useState } from "react";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false)
-
   return (
     <div>
       <Navbar />
@@ -18,8 +13,6 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      {alertVisible && <Alert onClose={()=>setAlertVisibility(false)}>This is an Alert</Alert>}
-      <Button onClick={()=> {setAlertVisibility(true)}}>Klik Disini</Button>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,55 @@
-import { Link } from "react-router-dom";
-
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" href="#">
           Navbar
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
-              </Link>
+              </a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/profile">
-                Profile
-              </Link>
+              <a className="nav-link" href="#">
+                Link
+              </a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
             </li>
           </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </div>
     </nav>
