123c7f2daf9c73b7f578123c8e7c851dbab195b7
RiyanTriadi <riyantriadii@gmail.com>
2025-10-23 18:35:28 +0700

    responsive changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,27 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  
   return (
     <>
-      <header className="flex justify-center text-white bg-black py-4">
-        <div className="container flex justify-between items-center">
-          <div className="text-2xl font-bold">Movie App</div>
+      <header className="fixed z-[99] w-screen text-white bg-black py-4">
+        <div className="w-[90%] lg:w-[1200] mx-auto flex justify-between items-center">
+          <div className="text-2xl font-bold">
+            <Link to="/">Movie App</Link>{" "}
+          </div>
+
           <nav className="hidden gap-5 items-center md:flex">
             <ul className="flex gap-5">
-              <li className="nav-link">
+              <li className="hover:text-gray-300 transition-colors">
                 <Link to="/">Home</Link>
               </li>
-              <li className="nav-link">
+              <li className="hover:text-gray-300 transition-colors">
                 <Link to="/category">Category</Link>
               </li>
-              <li className="nav-link">
+              <li className="hover:text-gray-300 transition-colors">
                 <Link to="/browse">Browse Movie</Link>
               </li>
             </ul>
-            <div className="quick-search">
+            <div>
               <form action="">
                 <input
                   type="text"
