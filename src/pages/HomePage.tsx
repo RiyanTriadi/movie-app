4d12f4e2c46f537535e69623a4e9907a08931d7c
RiyanTriadi <riyantriadii@gmail.com>
2025-10-27 11:51:47 +0700

    Refactor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
-import MovieList from "../components/MovieList";
+import MovieList from "../components/HomePage/MovieList";
+import TrendList from "../components/HomePage/TrendList";
 
 const HomePage = () => {
+
+  
   return (
     <>
-      <div className="w-[1200px] m-auto pt-20">
-        <MovieList />
+      <div className="m-auto pt-20 px-6">
+        <TrendList />
+        <MovieList/>
       </div>
     </>
   );
