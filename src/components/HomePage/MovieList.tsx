8b4d72efb88267f6af56b934c07ad1599dcf721d
RiyanTriadi <riyantriadii@gmail.com>
2025-10-27 11:51:18 +0700

    Responsive Update

---
+++ src/components/HomePage/MovieList.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import type { TMDbApiResponse, TMDbMovie } from "../../types.tsx";
+import MovieCard from "../MovieCard.tsx";
+
+const MovieList = () => {
+  const [movies, setMovies] = useState<TMDbMovie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchNowPlayingMovies = async () => {
+      try {
+        const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error("Failed to get data.");
+        }
+
+        const data: TMDbApiResponse = await response.json();
+        setMovies(data.results.slice(0, 20));
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchNowPlayingMovies();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>An error occurred: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="text-2xl md:text-4xl font-bold pb-4">Movie List</h1>
+      {/* 
+        - grid-cols-3: 3 kolom di layar terkecil (mobile).
+        - sm:grid-cols-4: 4 kolom di layar 'small' (tablet portrait).
+        - md:grid-cols-5: 5 kolom di layar 'medium' dan lebih besar (desktop).
+      */}
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-2 md:gap-6">
+        {movies.map((movie)=>(<MovieCard key={movie.id} movie={movie}/>))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
